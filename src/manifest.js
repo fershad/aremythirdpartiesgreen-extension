74c479a86a5b7b89f9ad0dd4449f4fda7f1a031d
fershad <27988517+fershad@users.noreply.github.com>
2023-12-19 17:53:16 +0800

    remove unused code

--- src/manifest.js
+++ src/manifest.js
@@ -6,6 +6,7 @@ export default defineManifest({
   description: packageData.description,
   version: packageData.version,
   manifest_version: 3,
+  // default_locale: 'en',
   icons: {
     16: 'img/logo-16.png',
     32: 'img/logo-34.png',
@@ -18,25 +19,26 @@ export default defineManifest({
   },
   // options_page: 'options.html',
   // devtools_page: 'devtools.html',
-  background: {
-    service_worker: 'src/background/index.js',
-    type: 'module',
-  },
-  content_scripts: [
-    {
-      matches: ['http://*/*', 'https://*/*'],
-      js: ['src/contentScript/index.js'],
-    },
-  ],
+  // background: {
+  //   service_worker: 'src/background/index.js',
+  //   scripts: ['src/utils/ext.js', 'src/background/index.js'],
+  //   type: 'module',
+  // },
+  // content_scripts: [
+  //   {
+  //     matches: ['http://*/*', 'https://*/*'],
+  //     js: ['src/contentScript/index.js'],
+  //   },
+  // ],
   side_panel: {
     default_path: 'sidepanel.html',
   },
-  web_accessible_resources: [
-    {
-      resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png'],
-      matches: [],
-    },
-  ],
-  permissions: ['sidePanel', "tabs",  "scripting",  "activeTab"],
+  // web_accessible_resources: [
+  //   {
+  //     resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png'],
+  //     matches: ['http://*/*', 'https://*/*'],
+  //   },
+  // ],
+  permissions: ["sidePanel", "tabs",  "scripting",  "activeTab"],
   host_permissions: ['<all_urls>'],
 })
